e6520f1c4f6db29018176166f48812241d9e3f7f
David O <davidoak@users.noreply.github.com>
2018-06-05 12:28:37 -0700

    Create inter_string.c

---
+++ 3D/inter_string.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_inter(char *str1, char *str2, int *asc)
+{
+	while (*str2)
+		asc[(int)(*(str2++))] = 1;
+	while (*str1)
+	{
+		if (asc[(int)(*str1)] == 1)
+		{
+			ft_putchar(*str1);
+			asc[(int)(*str1)] = 2;
+		}
+		str1++;
+	}
+	ft_putchar('\n');
+}
+
+int		main(int argc, char **argv)
+{
+	int		asc[128];
+
+	if (argc == 3)
+		ft_inter(argv[1], argv[2], asc);
+	else
+		ft_putchar('\n');
+	return (0);
+}
